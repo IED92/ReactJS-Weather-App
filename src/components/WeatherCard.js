664f0358e967f42ad021d2a41ac123c903459460
Ian <55965026+IED92@users.noreply.github.com>
2022-04-26 10:57:32 -0700

    ReactJS weather app
    ReactJS weather app hardcoded to show 5 day forecast of Vancouver. Done as a practice assessment.

---
+++ src/components/WeatherCard.js
@@ -0,0 +1,23 @@
+import { Card, CardContent, Typography } from '@mui/material'
+
+export default function WeatherCard({ date, maxTemp, minTemp, icon }) {
+
+    return (
+        <Card sx={{ minWidth: 175 }}>
+            <CardContent>
+                <Typography fontWeight='600'>{date}</Typography>
+                <img
+                    alt='weather icon'
+                    src={`http://openweathermap.org/img/wn/${icon}@2x.png`} />
+                <Typography variant="h5" component='div'>
+                    {maxTemp}&deg;
+                    <span style={{
+                        color: 'dimgray',
+                        marginLeft: '10px'
+                    }}>
+                        {minTemp}&deg;</span>
+                </Typography>
+            </CardContent>
+        </Card>
+    );
+}
---
+++ src/components/WeatherContainer.js
@@ -0,0 +1,67 @@
+import { Box } from '@mui/system';
+import { useState, useEffect } from 'react';
+import Loader from './Loader';
+import WeatherCard from './WeatherCard';
+import * as dayjs from 'dayjs'
+
+const kelvinToC = (kTemp) => {
+    return (kTemp - 273.15).toFixed(0);
+};
+
+const myFunc = (data) => Array.from(data).slice(0, 5)
+
+const unixToDate = (data) => dayjs.unix(data).format('ddd')
+
+export default function WeatherContainer() {
+
+    const [isLoading, setIsLoading] = useState({});
+    const [apiData, setApiData] = useState({});
+
+    const lat = '49.2827'
+    const long = '-123.1207'
+
+    const apiKey = '1ae5b3a58a1d2fd2112f37b2a018d14b'
+    const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${apiKey}`;
+
+    useEffect(() => {
+        fetch(apiUrl)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log('data', data)
+                // transform the data here
+                const normalizedData = myFunc(data.daily);
+                // set the data into setAPI
+                setApiData(normalizedData)
+                return normalizedData;
+            })
+            .catch(e => console.log(e))
+            .finally(() => {
+                console.log('setting to false');
+                setIsLoading(false)
+            })
+    }, [apiUrl]);
+
+    useEffect(() => {
+        console.log('data', apiData)
+
+    }, [apiData])
+
+
+
+    const WeatherItems = () => {
+        return apiData.map(item => 
+        <WeatherCard 
+            maxTemp={kelvinToC(item.temp.max)} 
+            minTemp={kelvinToC(item.temp.min)}
+            icon={item.weather[0].icon}
+            date={unixToDate(item.dt)}
+            data={item} />)
+    }
+
+    return (
+        <Box sx={{display: 'flex'}}>
+            {isLoading ? <Loader /> : <WeatherItems />}
+        </Box>
+    )
+
+}
